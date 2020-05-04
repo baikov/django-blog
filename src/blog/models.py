ca67274d974ffe958a52930aa6b59680459735a5
Alex Baikov <baikov52@gmail.com>
2020-05-04 15:06:51 +0300

    fix slug field

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -6,11 +7,14 @@ class Post(models.Model):
     image = models.ImageField(blank=True)
     seo_title = models.CharField(max_length=200, blank=True)
     seo_description = models.CharField(max_length=250, blank=True)
-    slug = models.SlugField()
+    slug = models.SlugField(blank=True)
     body = models.TextField()
     created_date = models.DateField(auto_now_add=True)
     updated_date = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'slug': self.slug})
 
