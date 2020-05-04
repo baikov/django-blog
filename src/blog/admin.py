045958969df73c905a3b5a659483959ba2acaac6
Alex Baikov <baikov52@gmail.com>
2020-05-04 14:59:35 +0300

    extended display of posts in the admin panel

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Post
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'slug')
+    list_filter = ['created_date']
+    search_fields = ['title']
+    fieldsets = [
+        (None, {'fields': ['title', 'short_text', 'image', 'body']}),
+        ('SEO', {'fields': ['slug', 'seo_title', 'seo_description']}),
+    ]
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
