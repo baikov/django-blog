3fcfef93a75e14a11cb1d64ff7537a1601de94e8
Alex Baikov <baikov52@gmail.com>
2020-05-15 23:05:35 +0300

    add edit path and admin fix

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -7,4 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='list'),
     path('<slug:slug>/', views.PostDetailView.as_view(), name='detail'),
+    path('<slug:slug>/edit/', views.PostEditView.as_view(), name='edit'),
+    # path('<slug:slug>/', views.blog_detail, name='detail'),
 ]
